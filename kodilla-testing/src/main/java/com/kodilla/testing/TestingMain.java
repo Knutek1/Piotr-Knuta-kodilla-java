0b922db113ac7031327179f04b4a5ee9ed3445f6
Knutek1 <piotr.knuta@gmail.com>
2024-01-09 12:09:18 +0100

    third commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -9,17 +9,16 @@ public class TestingMain {
         // zrobiłem wypisanie na ekran, dla sprawdzenia zwracanego Stringa samogłoskowego
         System.out.println(vowels(word1));
     }
-    public static String vowels(String word){
+
+    public static String vowels(String word) {
         char tab[] = word.toCharArray();
         StringBuilder sb = new StringBuilder();
 
-        for(char i : tab) {
-            if(i == 'a' || i == 'e'|| i == 'i'||i == 'o'|| i == 'u'|| i == 'y' || i == 'A' || i == 'E'|| i == 'I'||i == 'O'|| i == 'U'|| i == 'Y') {
+        for (char i : tab) {
+            if (i == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u' || i == 'y' || i == 'A' || i == 'E' || i == 'I' || i == 'O' || i == 'U' || i == 'Y') {
                 sb.append(i);
             }
         }
-       return sb.toString();
+        return sb.toString();
     }
-
-
 }
