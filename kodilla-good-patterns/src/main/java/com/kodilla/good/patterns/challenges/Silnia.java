77a88a5d7f4390c94892357e6a50c95f23be707b
Knutek1 <piotr.knuta@gmail.com>
2024-02-06 11:18:58 +0100

    13.1 Zadanie: funkcyjna iteracja po tytułach + silnia

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Silnia.java
@@ -0,0 +1,12 @@
+package com.kodilla.good.patterns.challenges;
+
+public class Silnia {
+    public long factorial(int n){
+        int factorial = 1;
+        for(int i=1;i<=n;i++){
+            factorial = factorial*i;
+        }
+
+        return factorial;
+    }
+}
