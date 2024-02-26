30ea8e979d8030a7346e26c1822ac3796b2f141a
Knutek1 <piotr.knuta@gmail.com>
2024-02-26 14:26:03 +0100

    15.6 Zadanie: Rekurencyjne ciągotki Fibonacciego

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Fibonacci.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Fibonacci.java
@@ -2,13 +2,25 @@ package com.kodilla.spring.portfolio;
 
 public class Fibonacci {
 
-    public int fibonacciCalculate(int n){
+    public int fibonacciCalculateIterative(int n) {
         int fibonacciArray[] = new int[n];
-        fibonacciArray[0]=0;
-        fibonacciArray[1]=1;
-        for (int i=1; i<n-1; i++){
-           fibonacciArray[i+1] = fibonacciArray[i]+fibonacciArray[i-1];
+        fibonacciArray[0] = 0;
+        fibonacciArray[1] = 1;
+        for (int i = 1; i < n - 1; i++) {
+            fibonacciArray[i + 1] = fibonacciArray[i] + fibonacciArray[i - 1];
         }
-        return fibonacciArray[n-1];
+        return fibonacciArray[n - 1];
+    }
+
+    public int fibonacciCalculateRecurrent(int n){
+        if(n==1){
+            return 0;
+        }
+        if(n==2 || n==3){
+            return 1;
+        }
+
+            return fibonacciCalculateRecurrent(n - 1) + fibonacciCalculateRecurrent(n - 2);
+
     }
 }
--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/FibonacciRunner.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/FibonacciRunner.java
@@ -3,7 +3,7 @@ package com.kodilla.spring.portfolio;
 public class FibonacciRunner {
     public static void main(String[] args) {
         Fibonacci fibonacci = new Fibonacci();
-
-        System.out.println(fibonacci.fibonacciCalculate(9));
+        System.out.println(fibonacci.fibonacciCalculateIterative(9));
+        System.out.println(fibonacci.fibonacciCalculateRecurrent(9));
     }
 }
